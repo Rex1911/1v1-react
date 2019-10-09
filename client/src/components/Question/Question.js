b16a3156b37d95ed73eedd539a42e5a68e2d4f83
Aryan Likhite <aryanlikhite@gmail.com>
2019-10-10 01:58:03 +0530

    Added warping text in Questions component

--- client/src/components/Question/Question.js
+++ client/src/components/Question/Question.js
@@ -17,19 +17,19 @@ export default props => {
             </Typography>
             <div id="questionArea">
                 <Typography variant="subtitle2">QUESTION</Typography>
-                <pre style={{fontSize: 15}}>{questionData.main}</pre>
+                <pre style={{fontSize: 15, whiteSpace: "pre-wrap"}}>{questionData.main}</pre>
                 <Typography variant="subtitle2">INPUT FORMAT</Typography>
-                <pre style={{fontSize: 15}}>{questionData.input}</pre>
+                <pre style={{fontSize: 15, whiteSpace: "pre-wrap"}}>{questionData.input}</pre>
                 <Typography variant="subtitle2">OUTPUT FORMAT</Typography>
-                <pre style={{fontSize: 15}}>{questionData.output}</pre>
+                <pre style={{fontSize: 15, whiteSpace: "pre-wrap"}}>{questionData.output}</pre>
                 <Typography variant="subtitle2">CONSTRAINTS</Typography>
-                <pre style={{fontSize: 15}}>{questionData.constraints}</pre>
+                <pre style={{fontSize: 15, whiteSpace: "pre-wrap"}}>{questionData.constraints}</pre>
                 <Typography variant="subtitle2">INPUT EXAMPLE</Typography>
                 <pre style={{fontSize: 15, backgroundColor:"#EAECEE", padding: 10}}>{questionData.exampleIp}</pre>
                 <Typography variant="subtitle2">OUTPUT EXAMPLE</Typography>
                 <pre style={{fontSize: 15, backgroundColor:"#EAECEE", padding: 10}}>{questionData.exampleOp}</pre>
                 <Typography variant="subtitle2">EXPLANATION</Typography>
-                <pre style={{fontSize: 15}}>{questionData.explanation}</pre>
+                <pre style={{fontSize: 15, whiteSpace: "pre-wrap"}}>{questionData.explanation}</pre>
             </div>
         </Paper>
     );
